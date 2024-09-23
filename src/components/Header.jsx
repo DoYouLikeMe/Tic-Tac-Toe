19acdefc933c7fed70b4af2f37dc40fb14a0e95c
ALEXEY <avoronsib@gmail.com>
2024-09-23 10:50:44 +0700

    add basic structure and styling

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <div className="header">
+      <div class="header__title">Tic Tac Toe</div>
+      <div class="header__instruction">
+        View Prototype, pick a player and press the keys to start playing
+      </div>
+    </div>
+  );
+}
