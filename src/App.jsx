470c3414924be2cb2fa33dc109c0c698b180d7bf
ALEXEY <avoronsib@gmail.com>
2024-09-27 11:44:30 +0700

    basic gameover logic

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import GameField from "./components/GameField";
 import {useState, useEffect} from "react";
 import RestartButton from "./components/RestartButton";
+import gameBoard from "./components/GameField";
 let gameBoardData = [
   [null, null, null],
   [null, null, null],
@@ -22,6 +23,8 @@ function App() {
     },
   });
   const [currentPlayer, setCurrentPlayer] = useState("X");
+  let winner = null;
+  let isDraw = false;
   const handleKeys = (event) => {
     switch (event.code) {
       case "KeyR":
@@ -51,6 +54,7 @@ function App() {
       setCurrentPlayer((curentPlayer) => {
         return curentPlayer === "X" ? "O" : "X";
       });
+      handleGameOver(gameBoardData);
       return updateTurns;
     });
   };
@@ -59,11 +63,48 @@ function App() {
     setGameTurns([]);
     setCurrentPlayer("X");
   };
+  const handleGameOver = (gameboard) => {
+    const verticalLinesCombination = gameboard.map((line) =>
+      Array.from(new Set(line))
+    );
+    const horizontalLinesCombination = gameboard.map((line, index) => {
+      const simbols = [];
+      gameboard.forEach((line) => simbols.push(line[index]));
+      return Array.from(new Set(simbols));
+    });
+
+    const crossLinesLeftCombination = Array.from(
+      new Set([gameboard[0][0], gameboard[1][1], gameboard[2][2]])
+    );
+
+    const crossLinesRightCombination = Array.from(
+      new Set([gameboard[0][2], gameboard[1][1], gameboard[2][0]])
+    );
+
+    const winningCombination = [
+      verticalLinesCombination,
+      horizontalLinesCombination,
+      [crossLinesLeftCombination, crossLinesRightCombination],
+    ]
+      .flat()
+      .filter(
+        (combination) => combination.length === 1 && !combination.includes(null)
+      );
+
+    if (winningCombination.length) winner = currentPlayer;
+    else if (!gameboard.flat().includes(null)) isDraw = true;
+
+    console.log(isDraw, winner);
+  };
   return (
     <>
       <Header />
       <GameField handleGameTurns={handleGameTurns} gameBoard={gameBoardData} />
-      <RestartButton restartHandler={restartGame}>
+      <RestartButton
+        winner={winner}
+        isDraw={isDraw}
+        restartHandler={restartGame}
+      >
         R - Restart Game
       </RestartButton>
     </>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
 export default function Header() {
   return (
     <div className="header">
-      <div class="header__title">Tic Tac Toe</div>
-      <div class="header__instruction">
+      <div className="header__title">Tic Tac Toe</div>
+      <div className="header__instruction">
         View Prototype, pick a player and press the keys to start playing
       </div>
     </div>
